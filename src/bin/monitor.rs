5e957d3599d912a8207cb12d973f13762e0ff6e5
P. Todd Decker <ptdecker@mac.com>
2023-06-21 23:12:06 -0500

    Started VM struct including RAM

--- src/bin/monitor.rs
+++ src/bin/monitor.rs
@@ -1,14 +1,29 @@
+//! Universal Retro Monitor
+//!
+
+use array_init::array_init;
 use console::Term;
+
+#[derive(Debug)]
+struct VirtualMachine {
+    ram: [u8; 65536],
+}
+
 fn main() {
+    let mut vm = VirtualMachine {
+        ram: array_init(|_| 0),
+    };
     let term = Term::stdout();
     term.clear_screen().unwrap();
     term.write_line("Universal Retro Monitor v0.1").unwrap();
     term.write_str(":").unwrap();
     loop {
         match term.read_char() {
-            Ok(ch) if ch == 'Q' => break,
+            Ok(ch) => match ch.to_ascii_uppercase() {
+                'Q' => break,
+                _ => continue,
+            },
             Err(_) => unimplemented!("Unattended terminal error handler not implemented"),
-            _ => (),
         }
     }
     term.clear_screen().unwrap();
